6f1e17ea807320544fb58c4502d8b28852e510a8
Dmitrii Shatokhin <dmtrshatokhin@gmail.com>
2024-12-23 04:57:48 +0100

    chore: update grimoire_css to version 1.2.0 and modify start function to use start_as_cli

--- gcssjs/src/lib.rs
+++ gcssjs/src/lib.rs
@@ -1,13 +1,11 @@
 #![deny(clippy::all)]
 
-use grimoire_css_lib;
-
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
 pub fn start(mode: String) {
-  if let Err(e) = grimoire_css_lib::start(mode) {
+  if let Err(e) = grimoire_css_lib::start_as_cli(vec![String::new(), mode]) {
     eprintln!("Error: {}", e);
   }
 }
