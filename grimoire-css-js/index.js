5314aaedbec44ea4cf8ba08f454e9fb2197d8d2e
Dmitrii Shatokhin <dmtrshatokhin@gmail.com>
2025-07-18 15:19:09 +0200

    chore: rename gcssjs to grimoire-css-js

--- grimoire-css-js/index.js
+++ grimoire-css-js/index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'gcssjs.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'grimoire-css-js.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./gcssjs.android-arm64.node')
+            nativeBinding = require('./grimoire-css-js.android-arm64.node')
           } else {
-            nativeBinding = require('@persevie/gcssjs-android-arm64')
+            nativeBinding = require('@persevie/grimoire-css-js-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'gcssjs.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'grimoire-css-js.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./gcssjs.android-arm-eabi.node')
+            nativeBinding = require('./grimoire-css-js.android-arm-eabi.node')
           } else {
-            nativeBinding = require('@persevie/gcssjs-android-arm-eabi')
+            nativeBinding = require('@persevie/grimoire-css-js-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -63,13 +63,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'gcssjs.win32-x64-msvc.node')
+          join(__dirname, 'grimoire-css-js.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./gcssjs.win32-x64-msvc.node')
+            nativeBinding = require('./grimoire-css-js.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@persevie/gcssjs-win32-x64-msvc')
+            nativeBinding = require('@persevie/grimoire-css-js-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -77,13 +77,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'gcssjs.win32-ia32-msvc.node')
+          join(__dirname, 'grimoire-css-js.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./gcssjs.win32-ia32-msvc.node')
+            nativeBinding = require('./grimoire-css-js.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@persevie/gcssjs-win32-ia32-msvc')
+            nativeBinding = require('@persevie/grimoire-css-js-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,13 +91,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'gcssjs.win32-arm64-msvc.node')
+          join(__dirname, 'grimoire-css-js.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./gcssjs.win32-arm64-msvc.node')
+            nativeBinding = require('./grimoire-css-js.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@persevie/gcssjs-win32-arm64-msvc')
+            nativeBinding = require('@persevie/grimoire-css-js-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -108,23 +108,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'gcssjs.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'grimoire-css-js.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./gcssjs.darwin-universal.node')
+        nativeBinding = require('./grimoire-css-js.darwin-universal.node')
       } else {
-        nativeBinding = require('@persevie/gcssjs-darwin-universal')
+        nativeBinding = require('@persevie/grimoire-css-js-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'gcssjs.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'grimoire-css-js.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./gcssjs.darwin-x64.node')
+            nativeBinding = require('./grimoire-css-js.darwin-x64.node')
           } else {
-            nativeBinding = require('@persevie/gcssjs-darwin-x64')
+            nativeBinding = require('@persevie/grimoire-css-js-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -132,13 +132,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'gcssjs.darwin-arm64.node')
+          join(__dirname, 'grimoire-css-js.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./gcssjs.darwin-arm64.node')
+            nativeBinding = require('./grimoire-css-js.darwin-arm64.node')
           } else {
-            nativeBinding = require('@persevie/gcssjs-darwin-arm64')
+            nativeBinding = require('@persevie/grimoire-css-js-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -152,12 +152,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'gcssjs.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'grimoire-css-js.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./gcssjs.freebsd-x64.node')
+        nativeBinding = require('./grimoire-css-js.freebsd-x64.node')
       } else {
-        nativeBinding = require('@persevie/gcssjs-freebsd-x64')
+        nativeBinding = require('@persevie/grimoire-css-js-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -168,26 +168,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'gcssjs.linux-x64-musl.node')
+            join(__dirname, 'grimoire-css-js.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./gcssjs.linux-x64-musl.node')
+              nativeBinding = require('./grimoire-css-js.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@persevie/gcssjs-linux-x64-musl')
+              nativeBinding = require('@persevie/grimoire-css-js-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'gcssjs.linux-x64-gnu.node')
+            join(__dirname, 'grimoire-css-js.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./gcssjs.linux-x64-gnu.node')
+              nativeBinding = require('./grimoire-css-js.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@persevie/gcssjs-linux-x64-gnu')
+              nativeBinding = require('@persevie/grimoire-css-js-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -197,26 +197,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'gcssjs.linux-arm64-musl.node')
+            join(__dirname, 'grimoire-css-js.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./gcssjs.linux-arm64-musl.node')
+              nativeBinding = require('./grimoire-css-js.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@persevie/gcssjs-linux-arm64-musl')
+              nativeBinding = require('@persevie/grimoire-css-js-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'gcssjs.linux-arm64-gnu.node')
+            join(__dirname, 'grimoire-css-js.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./gcssjs.linux-arm64-gnu.node')
+              nativeBinding = require('./grimoire-css-js.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@persevie/gcssjs-linux-arm64-gnu')
+              nativeBinding = require('@persevie/grimoire-css-js-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -226,26 +226,26 @@ switch (platform) {
       case 'arm':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'gcssjs.linux-arm-musleabihf.node')
+            join(__dirname, 'grimoire-css-js.linux-arm-musleabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./gcssjs.linux-arm-musleabihf.node')
+              nativeBinding = require('./grimoire-css-js.linux-arm-musleabihf.node')
             } else {
-              nativeBinding = require('@persevie/gcssjs-linux-arm-musleabihf')
+              nativeBinding = require('@persevie/grimoire-css-js-linux-arm-musleabihf')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'gcssjs.linux-arm-gnueabihf.node')
+            join(__dirname, 'grimoire-css-js.linux-arm-gnueabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./gcssjs.linux-arm-gnueabihf.node')
+              nativeBinding = require('./grimoire-css-js.linux-arm-gnueabihf.node')
             } else {
-              nativeBinding = require('@persevie/gcssjs-linux-arm-gnueabihf')
+              nativeBinding = require('@persevie/grimoire-css-js-linux-arm-gnueabihf')
             }
           } catch (e) {
             loadError = e
@@ -255,26 +255,26 @@ switch (platform) {
       case 'riscv64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'gcssjs.linux-riscv64-musl.node')
+            join(__dirname, 'grimoire-css-js.linux-riscv64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./gcssjs.linux-riscv64-musl.node')
+              nativeBinding = require('./grimoire-css-js.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('@persevie/gcssjs-linux-riscv64-musl')
+              nativeBinding = require('@persevie/grimoire-css-js-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'gcssjs.linux-riscv64-gnu.node')
+            join(__dirname, 'grimoire-css-js.linux-riscv64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./gcssjs.linux-riscv64-gnu.node')
+              nativeBinding = require('./grimoire-css-js.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('@persevie/gcssjs-linux-riscv64-gnu')
+              nativeBinding = require('@persevie/grimoire-css-js-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -283,13 +283,13 @@ switch (platform) {
         break
       case 's390x':
         localFileExisted = existsSync(
-          join(__dirname, 'gcssjs.linux-s390x-gnu.node')
+          join(__dirname, 'grimoire-css-js.linux-s390x-gnu.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./gcssjs.linux-s390x-gnu.node')
+            nativeBinding = require('./grimoire-css-js.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('@persevie/gcssjs-linux-s390x-gnu')
+            nativeBinding = require('@persevie/grimoire-css-js-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
